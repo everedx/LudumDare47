6b7ef27aaf92a1b39fbaeccecb294eadd5a7fb0d
pmartinez <pablo.martinez@evicertia.com>
2020-10-03 13:06:25 +0200

    Restrict player movement to screen bounds

--- Assets/Scripts/Player/SpaceshipHandler.cs
+++ Assets/Scripts/Player/SpaceshipHandler.cs
@@ -10,6 +10,8 @@ public class SpaceshipHandler : MonoBehaviour
 
 	// TODO: Convert this to an array/list of the currently held shooters
 	private IShooter _shooter;
+	private Camera _mainCamera;
+	private Rect _halfSpriteRect;
 
 	private int movementX;
 	private int movementY;
@@ -17,6 +19,10 @@ public class SpaceshipHandler : MonoBehaviour
 	void Start()
 	{
 		_shooter = new SimpleShooter();
+		_mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+		_halfSpriteRect = new Rect(GetComponent<SpriteRenderer>().sprite.rect);
+		_halfSpriteRect.width /= 2;
+		_halfSpriteRect.height /= 2;
 	}
 
 	void FixedUpdate()
@@ -41,6 +47,14 @@ public class SpaceshipHandler : MonoBehaviour
 		if (Input.GetKey(left)) movementY = -1;
 		else if (Input.GetKey(right)) movementY = 1;
 		else movementY = 0;
+
+		var screenPos = _mainCamera.WorldToScreenPoint(transform.position);
+
+		// Pressing Right creates movement in the positive Y coordinates... makes these conditions a bit weird, fix it somehow if it becomes too annoying
+		if (screenPos.x - _halfSpriteRect.width < 0 && movementY < 0) movementY = 0;
+		else if (screenPos.x + _halfSpriteRect.width > _mainCamera.pixelWidth && movementY > 0) movementY = 0;
+		if (screenPos.y - _halfSpriteRect.height < 0 && movementX > 0) movementX = 0;
+		else if (screenPos.y + _halfSpriteRect.height > _mainCamera.pixelHeight && movementX < 0) movementX = 0;
 
 		_shooter.FromCurrentShootingState(Input.GetKeyDown(shoot), Input.GetKeyUp(shoot), Input.GetKey(shoot), gameObject);
 	}
