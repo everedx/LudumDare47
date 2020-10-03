de1a7b327c077cd2266752a50e66fffb33c39b2f
pmartinez <pablo.martinez@evicertia.com>
2020-10-03 04:11:27 +0200

    Initial ship control and pathing

---
+++ Assets/CameraFollower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+	public float YOffset;
+	public float XOffset;
+	public GameObject ToFollow;
+	Transform _transformToFollow;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		_transformToFollow = ToFollow.transform;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.rotation = _transformToFollow.rotation;
+		transform.up = -transform.up;
+		transform.Rotate(Vector3.back, 90, Space.Self);
+		transform.position = new Vector3(_transformToFollow.position.x, _transformToFollow.position.y, transform.position.z);
+		transform.Translate(new Vector3(XOffset, YOffset, 0), Space.Self);
+	}
+}
---
+++ Assets/SpaceshipHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipHandler : MonoBehaviour
+{
+	public float MovementSpeed = 5;
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	void FixedUpdate()
+	{
+		int movementX = 0;
+		if (Input.GetKey(KeyCode.UpArrow)) movementX = -1;
+		else if (Input.GetKey(KeyCode.DownArrow)) movementX = 1;
+
+		int movementY = 0;
+		if (Input.GetKey(KeyCode.LeftArrow)) movementY = -1;
+		else if (Input.GetKey(KeyCode.RightArrow)) movementY = 1;
+
+		transform.Translate(movementX * MovementSpeed * Time.fixedDeltaTime, movementY * MovementSpeed * Time.fixedDeltaTime, 0, Space.Self);
+	}
+}
