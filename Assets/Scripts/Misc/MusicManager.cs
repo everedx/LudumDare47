d4295d286325fbe939093b5ca23a2dd75ebdd371
everedx <carlos.serranoe1992@gmail.com>
2020-10-05 12:31:17 +1100

    Music Loop done

---
+++ Assets/Scripts/Misc/MusicManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+
+    [SerializeField] AudioSource source1;
+    [SerializeField] AudioSource source2;
+    [SerializeField] float durationInSeconds;
+
+    int iterationIndex;
+    AudioSource activeSource;
+
+    float timer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        iterationIndex = 0;
+        activeSource = source1;
+        activeSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (Math.Floor(Time.time / durationInSeconds) > iterationIndex)
+        {
+            if (activeSource == source1)
+                activeSource = source2;
+            else
+                activeSource = source1;
+
+            activeSource.Play();
+            iterationIndex++;
+        }
+
+    }
+}
