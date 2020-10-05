1a12ec9ccf744bb75512b047e2a65646958b1a6a
everedx <carlos.serranoe1992@gmail.com>
2020-10-06 07:26:58 +1100

    PauseMenu

---
+++ Assets/PauseController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ResumeGame()
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<SpaceshipHandler>().ResumeGame();
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void GotoMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
